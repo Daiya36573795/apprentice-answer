fdcf1d8f37e50cc5e561324564b21da39d2a4493
Daiya36573795 <m.daiya123@icloud.com>
2024-09-23 15:41:39 +0900

    step2の実装が完了

--- password_manager.sh
+++ password_manager.sh
@@ -1,18 +1,53 @@
 #!/bin/bash
 
-# ファイルの保存先を指定
+# 保存先ファイルを指定
 FILE="passwords.txt"
 
-# パスワードマネージャーのメッセージを表示
-echo "パスワードマネージャーへようこそ！"
+# パスワードを保存する関数
+add_password() {
+    read -p "サービス名を入力してください: " service_name
+    read -p "ユーザー名を入力してください: " username
+    read -sp "パスワードを入力してください: " password
+    echo # 改行
+    # 情報をファイルに保存
+    echo "$service_name:$username:$password" >> $FILE
+    echo "パスワードの追加は成功しました。"
+}
 
-# 入力を促す
-read -p "サービス名を入力してください: " service_name
-read -p "ユーザー名を入力してください: " username
-read -sp "パスワードを入力してください: " password
+# パスワードを取得する関数
+get_password() {
+    read -p "サービス名を入力してください: " service_name
+    # 指定されたサービス名がファイルにあるか検索
+    result=$(grep "^$service_name:" $FILE)
+    if [ -z "$result" ]; then
+        echo "そのサービスは登録されていません。"
+    else
+        # サービス情報を表示
+        IFS=":" read -r stored_service_name stored_username stored_password <<< "$result"
+        echo "サービス名：$stored_service_name"
+        echo "ユーザー名：$stored_username"
+        echo "パスワード：$stored_password"
+    fi
+}
 
-# 情報をファイルに追記
-echo "$service_name:$username:$password" >> $FILE
+# メインループ
+while true; do
+    echo "パスワードマネージャーへようこそ！"
+    read -p "次の選択肢から入力してください(Add Password/Get Password/Exit)：" action
 
-# 完了メッセージを表示
-echo "Thank you!"
+    case "$action" in
+        "Add Password")
+            add_password
+            ;;
+        "Get Password")
+            get_password
+            ;;
+        "Exit")
+            echo "Thank you!"
+            exit 0
+            ;;
+        *)
+            echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。"
+            ;;
+    esac
+done
