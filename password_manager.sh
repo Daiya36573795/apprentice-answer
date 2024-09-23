28811c7169b1fe4870741bd1599c7f46348ff17a
Daiya36573795 <m.daiya123@icloud.com>
2024-09-23 16:25:35 +0900

    step3の実装が完了

--- password_manager.sh
+++ password_manager.sh
@@ -1,24 +1,53 @@
 #!/bin/bash
 
-# 保存先ファイルを指定
+# 保存先ファイル
 FILE="passwords.txt"
+ENCRYPTED_FILE="passwords.txt.gpg"
+
+# パスフレーズ
+PASSPHRASE="encryption-password"
+
+# パスワードマネージャーのメッセージを表示
+echo "パスワードマネージャーへようこそ！"
 
 # パスワードを保存する関数
 add_password() {
     read -p "サービス名を入力してください: " service_name
     read -p "ユーザー名を入力してください: " username
     read -sp "パスワードを入力してください: " password
-    echo # 改行
-    # 情報をファイルに保存
-    echo "$service_name:$username:$password" >> $FILE
+    echo ""  # パスワード入力後に改行を挿入
+
+    # 暗号化ファイルが存在する場合は復号して内容を取得
+    if [ -f "$ENCRYPTED_FILE" ]; then
+        gpg --decrypt --batch --yes --passphrase "$PASSPHRASE" --output "$FILE" "$ENCRYPTED_FILE"
+    fi
+
+    # 新しい情報をファイルに追記
+    echo "$service_name:$username:$password" >> "$FILE"
+
+    # ファイルを暗号化
+    gpg --symmetric --batch --yes --passphrase "$PASSPHRASE" "$FILE"
+
+    # 暗号化後に元ファイルを削除
+    rm "$FILE"
+
     echo "パスワードの追加は成功しました。"
 }
 
 # パスワードを取得する関数
 get_password() {
     read -p "サービス名を入力してください: " service_name
-    # 指定されたサービス名がファイルにあるか検索
-    result=$(grep "^$service_name:" $FILE)
+
+    # ファイルが存在する場合のみ復号化
+    if [ -f "$ENCRYPTED_FILE" ]; then
+        gpg --decrypt --batch --yes --passphrase "$PASSPHRASE" --output "$FILE" "$ENCRYPTED_FILE"
+    else
+        echo "暗号化ファイルが存在しません。"
+        return
+    fi
+
+    # サービス名で検索
+    result=$(grep "^$service_name:" "$FILE")
     if [ -z "$result" ]; then
         echo "そのサービスは登録されていません。"
     else
@@ -28,6 +57,9 @@ get_password() {
         echo "ユーザー名：$stored_username"
         echo "パスワード：$stored_password"
     fi
+
+    # 復号化後に元のファイルを削除
+    rm "$FILE"
 }
 
 # メインループ
